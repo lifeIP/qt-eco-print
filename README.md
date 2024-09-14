985e10d41861d97747fb2f3caa2828a80520161e
lifeIP <molokeevartem@gmail.com>
2024-09-15 04:24:33 +0700

    Добавлена системная клавиатура

---
+++ README.md
@@ -0,0 +1,3 @@
+export QT_IM_MODULE=qtvirtualkeyboard
+find / -name libstdc++.so.6 2>/dev/null
+export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
--- src/Pages/PageReviewReceivedDocument/page_review_received_document.py
+++ src/Pages/PageReviewReceivedDocument/page_review_received_document.py
@@ -49,11 +49,17 @@ class PDFViewer(QWidget):
             pdf = PdfReader(filehandle)
             self.count_of_pages = len(pdf.pages)
 
+            nav = self.m_pdf_view.pageNavigator()
+            self.signal_page_nav_label.emit(nav.currentPage() + 1, self.count_of_pages)
+            
+
 
     Slot(int)
     def slot_page_selected(self, page):
         nav = self.m_pdf_view.pageNavigator()
         nav.jump(page, QPoint(), nav.currentZoom())
+
+        self.signal_page_nav_label.emit(nav.currentPage() + 1, self.count_of_pages)
 
 
     Slot()
@@ -63,13 +69,18 @@ class PDFViewer(QWidget):
         if nav.currentPage() > 0:
             nav.jump(nav.currentPage() - 1, QPoint(), nav.currentZoom())
 
+        self.signal_page_nav_label.emit(nav.currentPage() + 1, self.count_of_pages)
+
     Slot()
     def slot_next_page(self):
         nav = self.m_pdf_view.pageNavigator()
         if nav.currentPage() < self.count_of_pages - 1:
            nav.jump(nav.currentPage() + 1, QPoint(), nav.currentZoom())
 
+        self.signal_page_nav_label.emit(nav.currentPage() + 1, self.count_of_pages)
 
+
+    signal_page_nav_label = Signal(int, int)
 
     def __init__(self):
         super().__init__()
@@ -144,6 +155,13 @@ class PageReviewReceivedDocument(QWidget):
         self.signal_back_clicked.emit()
 
 
+
+
+    Slot(int, int)
+    def slot_page_nav_label(self, page, all_page):
+        self.label_page.setText(f"{page}/{all_page}")
+
+
     def initUI(self):
 
         self.btn_come_back = QPushButton("Назад")
@@ -152,23 +170,57 @@ class PageReviewReceivedDocument(QWidget):
         self.btn_prev_page = QPushButton()
         self.btn_prev_page.clicked.connect(self.slot_prev_page)
         self.btn_prev_page.setIcon(self.left_arrow_pixmap)
+        self.btn_prev_page.setIconSize(QSize(50, 50))
 
         self.btn_next_page = QPushButton()
         self.btn_next_page.clicked.connect(self.slot_next_page)
         self.btn_next_page.setIcon(self.right_arrow_pixmap)
+        self.btn_next_page.setIconSize(QSize(50, 50))
 
+        self.label_page = QLabel()
+
+        placeholder_v = QWidget()
+        placeholder_v.setSizePolicy(QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.Minimum)
+
+        placeholder_v_1 = QWidget()
+        placeholder_v_1.setSizePolicy(QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.Minimum)
+
+
+        self.e1 = QLineEdit()
+        self.e1.setValidator(QIntValidator())
+        self.e1.setMaxLength(4)
+        self.e1.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.e1.setFont(QFont("Arial",20))
         
+        self.e2 = QLineEdit()
+        self.e2.setValidator(QIntValidator())
+        self.e2.setMaxLength(4)
+        self.e2.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.e2.setFont(QFont("Arial",20))
+        
+        h_layout_1 = QHBoxLayout()
+        h_layout_1.addWidget(placeholder_v_1)
+        h_layout_1.addWidget(self.e1)
+        h_layout_1.addWidget(self.e2)
+
         h_layout = QHBoxLayout()
         h_layout.addWidget(self.btn_come_back)
+        h_layout.addWidget(placeholder_v)
         h_layout.addWidget(self.btn_prev_page)
+        h_layout.addWidget(self.label_page)
         h_layout.addWidget(self.btn_next_page)
 
 
+        v_layout = QVBoxLayout()
+        v_layout.addLayout(h_layout_1)
+        v_layout.addLayout(h_layout)
 
         self.m_pdf_viewer = PDFViewer()
         self.signal_next_page.connect(self.m_pdf_viewer.slot_next_page)
         self.signal_prev_page.connect(self.m_pdf_viewer.slot_prev_page)
+        self.m_pdf_viewer.signal_page_nav_label.connect(self.slot_page_nav_label)
         
         mainLayout = QVBoxLayout(self)
+        mainLayout.addLayout(v_layout)
         mainLayout.addWidget(self.m_pdf_viewer)
-        mainLayout.addLayout(h_layout)
+        
